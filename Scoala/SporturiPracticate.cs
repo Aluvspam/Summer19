3b36cd7e0b51f4d6bb83c910812a42c7936915b9
Geod11 <danilescugeorge@yahoo.com>
2019-09-05 20:01:52 +0300

    Clasa SportPracticat a fost creata partial

---
+++ Scoala/SporturiPracticate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scoala
+{
+    public class SporturiPracticate
+    {
+        public bool fotbal;
+        public bool baschet;
+        public bool volei;
+        public bool handbal;
+
+        public void SportPracticat ()
+        {
+            
+            if (fotbal == true)
+            {              
+                Console.WriteLine($"Sportul practicat de tine este fotbal");               
+            }
+            else if (baschet == true)
+            {
+
+                
+
+            }
+            
+
+
+
+        }
+
+
+    }
+}
