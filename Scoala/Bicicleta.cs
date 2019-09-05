2e4251850694c2ac15ea0cb763ef8c60782292fa
Gabriel Corlan <gabriel.corlan@outlook.com>
2019-09-05 19:44:22 +0300

    added new class Bicicleta

---
+++ Scoala/Bicicleta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scoala
+{
+    public class Bicicleta
+    {
+        public double MarimeRoata;
+        public string TipBicicleta;
+        public string MarcaBicicleta;
+    }
+}
