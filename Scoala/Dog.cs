c57449e3328b05c3c729ebb37394ac184bf7db3c
marian go <marian.gonta@gmail.com>
2019-10-17 19:52:02 +0300

    created a dog

---
+++ Scoala/Dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scoala
+{
+    class Dog
+    {
+        public Dog(string name, int age, bool isDomestic) : base(name, age, isDomestic)
+        {
+            public override void Eat()
+        {
+            Console.WriteLine("Running after cat !");
+        }
+            public override void Move()
+        {
+            Console.WriteLine("Running with my 4 legs");
+        }
+        public override void Slepp()
+        {
+            Console.WriteLine("Bone dream");
+        }
+        public override void Talk()
+        {
+            Console.WriteLine("I'm barking");
+        }
+
+
+        }
+    }
+}
