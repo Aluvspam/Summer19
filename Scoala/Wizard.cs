99ae0dea80275e7d95e9bd35c3266d09013b1737
andrei patrascu <andreipatrascu@outlook.com>
2019-10-17 19:55:58 +0300

    partial work on Wizard class

---
+++ Scoala/Wizard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scoala
+{
+    public class Wizard : Person
+    {
+        public Wizard(string nume, int age, Address adresa, Sex sex) : base(nume, age, adresa, sex)
+        {
+        }
+    }
+}
