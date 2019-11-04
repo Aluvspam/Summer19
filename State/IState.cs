81f2923617388f31472c9802335661adde973c93
Geod11 <danilescugeorge@yahoo.com>
2019-11-04 19:57:21 +0200

    State Interface

---
+++ State/IState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace State
+{
+    interface IState
+    {
+        public void Idle();
+        public void ClientPresent();
+        public void ThankYou();
+        public void OutOfOrder();
+
+    }
+}
