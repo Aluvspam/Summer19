7f2a39fc56c5debd457c0e203c6663c626aaf393
andrei patrascu <andreipatrascu@outlook.com>
2019-11-07 20:55:20 +0200

    added ChainOR

---
+++ ChainOR/HAnd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOR
+{
+    public class HAnd : AbstractHandle
+    {
+        char language = 'A';
+        public override void Handle(Message message)
+        {
+            if (message.Language == language)
+            {
+                Console.WriteLine("Eu, HAnd can handle this message: " + message.Text);
+            }
+            else
+            {
+                Handlers.Instance.GetNextHandler().Handle(message);
+            }
+        }
+    }
+}
---
+++ ChainOR/Handlers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOR
+{
+    public class Handlers
+    {
+        List<AbstractHandle> handlers;
+        int index;
+        static Handlers instance;
+        public static Handlers Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Handlers();
+                }
+                return instance;
+            }
+        }
+        public Handlers()
+        {
+            handlers = new List<AbstractHandle>();
+            index = 0;
+        }
+        public void AddHandle(AbstractHandle handler)
+        {
+            handlers.Add(handler);
+        }
+        public AbstractHandle GetNextHandler()
+        {
+            index++;
+            return index > handlers.Count ? null : handlers[index - 1];
+        }
+    }
+}
