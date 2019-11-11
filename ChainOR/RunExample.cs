95e95818f638f602a1113c68ff1b450e629d7842
andrei patrascu <andreipatrascu@outlook.com>
2019-11-11 20:13:33 +0200

    added another example and reset handlers index method

--- ChainOR/RunExample.cs
+++ ChainOR/RunExample.cs
@@ -11,7 +11,10 @@ namespace ChainOR
         public static void Run()
         {
             Handlers.Instance.AddHandle(new HAnd());
+            Handlers.Instance.AddHandle(new HandS());
             Handlers.Instance.GetNextHandler().Handle(new Message() { Text = "dublu vizor", Language = 'A' });
+            Handlers.Instance.ResetHandlersIndex();
+            Handlers.Instance.GetNextHandler().Handle(new Message() { Text = "Silvius language msg", Language = 'S' });
             Console.ReadLine();
         }
     }
