2efa6d5d3089e13ff7d9b143f3098ec395a21dd2
Geod11 <danilescugeorge@yahoo.com>
2019-11-22 23:07:57 +0200

    GeoPlayer class completed

--- TFT/GeoPlayer.cs
+++ TFT/GeoPlayer.cs
@@ -6,7 +6,62 @@ using System.Threading.Tasks;
 
 namespace TFT
 {
-    class GeoPlayer
-    {
-    }
+    public class GeoPlayer : IPlayer, IScore
+    {       
+         public int[] Scores
+         {
+            get
+            {
+                return new int[] { score1, score2 };
+            }
+         }
+        
+        Moves MyMove(Moves myLastMove, Moves othersLastMove)
+        {
+            
+            while (GeoPlayer == player1)
+	        {
+	         if (othersLastMove == Moves.B)	
+             {
+                return Moves.B;
+             }
+             else if (othersLastMove == Moves.C)
+	         {
+                return Moves.D;
+	         }
+             else if (othersLastMove == Moves.D)
+	         {
+                return Moves.D;
+	         } 
+	        }
+
+            while (GeoPlayer == player2)
+	        {
+             if (othersLastMove == Moves.B)	
+             {
+                return Moves.B;
+             }
+             else if (othersLastMove == Moves.C)
+	         {
+                return Moves.C;
+	         }
+             else if (othersLastMove == Moves.D)
+	         {
+                return Moves.C;
+	         } 	                    
+            }
+            
+        }
+        Moves FirstMove()
+        {
+            if (GeoPlayer == player1)
+	        {
+            return Moves.C;
+	        }
+            if (GeoPlayer == player2)
+	        {
+            return Moves.B;
+	        }
+        }
+	}
 }
