29ee5841fae1ffcc0b80a620c33b99f0eaacd522
Geod11 <danilescugeorge@yahoo.com>
2019-11-25 19:50:28 +0200

    Fixed my Player

--- TFT/GeoPlayer.cs
+++ TFT/GeoPlayer.cs
@@ -6,62 +6,27 @@ using System.Threading.Tasks;
 
 namespace TFT
 {
-    public class GeoPlayer : IPlayer, IScore
-    {       
-         public int[] Scores
-         {
-            get
-            {
-                return new int[] { score1, score2 };
-            }
-         }
-        
-        Moves MyMove(Moves myLastMove, Moves othersLastMove)
+    public class GeoPlayer : IPlayer
+    {             
+        public Moves MyMove(Moves myLastMove, Moves othersLastMove)
         {
-            
-            while (GeoPlayer == player1)
-	        {
+                       	        
 	         if (othersLastMove == Moves.B)	
              {
-                return Moves.B;
+                return myLastMove = Moves.D;
              }
              else if (othersLastMove == Moves.C)
 	         {
-                return Moves.D;
+                return myLastMove = Moves.D;
 	         }
-             else if (othersLastMove == Moves.D)
+             else
 	         {
-                return Moves.D;
-	         } 
-	        }
-
-            while (GeoPlayer == player2)
-	        {
-             if (othersLastMove == Moves.B)	
-             {
-                return Moves.B;
-             }
-             else if (othersLastMove == Moves.C)
-	         {
-                return Moves.C;
-	         }
-             else if (othersLastMove == Moves.D)
-	         {
-                return Moves.C;
-	         } 	                    
-            }
-            
+                return myLastMove = Moves.D;
+	         } 	                               
         }
-        Moves FirstMove()
+        public Moves FirstMove()
         {
-            if (GeoPlayer == player1)
-	        {
             return Moves.C;
-	        }
-            if (GeoPlayer == player2)
-	        {
-            return Moves.B;
-	        }
         }
 	}
 }
