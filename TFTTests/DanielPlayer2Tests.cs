0dcbd103658d2e60de58c74a6d54b87514982469
andrei patrascu <andreipatrascu@outlook.com>
2019-12-02 20:39:29 +0200

    added more tests

---
+++ TFTTests/DanielPlayer2Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TFT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFT.Tests
+{
+    [TestClass()]
+    public class DanielPlayer2Tests
+    {
+        [TestMethod()]
+        public void FirstMoveTest()
+        {
+            var player = new DanielPlayer2();
+            Assert.AreNotEqual(Moves.B, player.FirstMove());
+        }
+    }
+}
