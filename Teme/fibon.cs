53b53831a172684e2acf4a47fa78d96f91039328
Marian gonta <m.gontariu@hotmail.com>
2019-12-02 21:04:12 +0200

    -fibonaci

---
+++ Teme/fibon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teme
+{
+   public class fibon
+    {
+        public double Fib(int n)
+        {
+            if (n == 0)
+                return 0;
+            double rez = n + Fib(n - 1);
+            return rez;
+
+        }
+    }
+}
---
+++ TemeTests/fibonTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Teme;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teme.Tests
+{
+    [TestClass()]
+    public class fibonTests
+    {
+        [TestMethod()]
+        public void FibTest()
+
+        {
+            fibon a = new fibon();
+            Assert.AreEqual(6,4);
+        }
+    }
+}
